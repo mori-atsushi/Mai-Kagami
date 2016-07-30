df6269847bd78ddb5e8706619ba1a087de7c393b
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-07-30 11:13:32 +0900

    通し練習モードオプション画面、スピードの表示位置修正

--- Mai-Kagami/Mai-Kagami/ThroughOption.cpp
+++ Mai-Kagami/Mai-Kagami/ThroughOption.cpp
@@ -5,8 +5,9 @@ ThroughOptionButton::ThroughOptionButton(Font *font) {
 	button[1] = new Button(font, "DOWN", 2, 1);
 	button[2] = new Button(font, "スタート!", 0, 2);
 	button[3] = new Button(font, "戻る", 4);
-	speed[0] = new MyDrawText(font, "スピード", WIDTH * 0.72, HEIGHT * 0.5165, 0, 30);
-	speed[1] = new MyDrawText(font, "×1.0", WIDTH * 0.86, HEIGHT * 0.5165, 0, 30, "Yellow");
+	float height = BUTTON_POS + BUTTON_INTERVAL / 2;
+	speed[0] = new MyDrawText(font, "スピード", WIDTH * 0.72, height, 0, 30);
+	speed[1] = new MyDrawText(font, "×1.0", WIDTH * 0.86, height, 0, 30, "Yellow");
 	sp = 0;
 }
 
