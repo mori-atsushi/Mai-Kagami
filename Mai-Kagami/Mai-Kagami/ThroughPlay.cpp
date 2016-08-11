f1f3c7461fe40cc592d5c5fb3276cbe64a89b33f
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-08-11 09:22:59 +0900

    通し練習、カウントダウン画面再生三角形表示

--- Mai-Kagami/Mai-Kagami/ThroughPlay.cpp
+++ Mai-Kagami/Mai-Kagami/ThroughPlay.cpp
@@ -56,13 +56,26 @@ ThroughPlayBar::~ThroughPlayBar() {
 		delete circle[i];
 }
 
+//カウントダウン画面再生三角形
+PlayTriangle::PlayTriangle(const float x, const float y) 
+	: MyDrawTriangle("Yellow") {
+	const float w = 100, ex = 12;
+	float x1 = x - w + ex, x2 = x - w + ex, x3 = x + w + ex;
+	float y1 = y + w, y2 = y - w, y3 = y;
+	ChangePos(x1, y1, x2, y2, x3, y3);
+}
+
 //カウントダウン画面
 ThroughCountDown::ThroughCountDown(Font *font) {
+	const float x = WIDTH * 0.5; //円の中心（x座標）
+	const float y = HEIGHT * 0.5; //円の中心（y座標）
+	const float r = WIDTH * 0.2; //円の半径
 	blackBox = new BlackBox();
-	text = new MyDrawText(font, "準備をしてください", WIDTH * 0.5, HEIGHT * 0.7, 1, 40);
-	circle = new MyDrawCircle(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.2, "White");
-	countCircle1 = new MyDrawCircleGauge(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.2, 0, 4, "Yellow");
+	text = new MyDrawText(font, "準備をしてください", x, y + r + 80, 1, 40);
+	circle = new MyDrawCircle(x, y, r, "White");
+	countCircle1 = new MyDrawCircleGauge(x, y, r, 0, 4, "Yellow");
 	countCircle2 = new MyDrawCircle(0, 0, 12, "Yellow");
+	playTriangle = new PlayTriangle(x, y);
 }
 
 void ThroughCountDown::Update() {
@@ -80,6 +93,7 @@ void ThroughCountDown::View() {
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0); //透明度解除
 	countCircle1->View();
 	countCircle2->View();
+	playTriangle->View();
 }
 
 boolean ThroughCountDown::GetFlag() {
@@ -98,6 +112,7 @@ ThroughCountDown::~ThroughCountDown() {
 	delete circle;
 	delete countCircle1;
 	delete countCircle2;
+	delete playTriangle;
 }
 
 ThroughPlay::ThroughPlay(Font *font) {
