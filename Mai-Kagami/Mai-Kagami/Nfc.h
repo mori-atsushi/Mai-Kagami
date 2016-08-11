7af84406923829ac7f2213e5c02c741ba867cc53
kousuke nezu <kousuke.n2132@gmail.com>
2016-08-12 00:22:46 +0900

    CheckThread消去しました

--- Mai-Kagami/Mai-Kagami/Nfc.h
+++ Mai-Kagami/Mai-Kagami/Nfc.h
@@ -1,19 +1,33 @@
 #ifndef __NFC_H_INCLUDED__
 #define __NFC_H_INCLUDED__
 
+#include <WS2tcpip.h>
 #include <thread>
 #include"DxLib.h"
+#pragma comment(lib, "ws2_32.lib")
 
-class Nfc {
+#ifndef _RECVSTATUS_
+#define _RECVSTATUS_
+
+//受信状態
+enum RECVSTATUS
+{
+	RECV_STILL,		//データが来ていない
+	RECV_SUCCESSED,	//成功
+	RECV_FAILED,	//切断orエラー
+};
+#endif
+
+class Nfc 
+{
 public:
-	void Init();
-	int GetId(); //ユーザーIDの取得
+	void Init();	//初期化
+	int GetId();	//ユーザーIDの取得
 private:
-	void CheckThread(); //NFCをチェックするためのスレッド
+	IPDATA Ip;			//接続用IPアドレスデータ
 	int id;
-	IPDATA Ip;	//接続用IPアドレスデータ
-	int NetHandle;	//ネットワークハンドル
-	int DataLength;	//受信データ量保存用変数
+	int NetHandle;		//ネットワークハンドル
+	int DataLength;		//受信データ量保存用変数
 	char StrBuf[256];	//データバッファ
 };
 
