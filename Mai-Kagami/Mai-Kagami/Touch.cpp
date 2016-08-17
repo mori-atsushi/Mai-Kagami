7f8e064bd8c064548bc99df3ce7f8a2c293a1bf4
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-08-17 09:24:40 +0900

    Touchクラスを最適化

--- Mai-Kagami/Mai-Kagami/Touch.cpp
+++ Mai-Kagami/Mai-Kagami/Touch.cpp
@@ -2,30 +2,13 @@
 
 //Šm”F
 void Touch::Check() {
-	if (CheckHitKey(KEY_INPUT_1))
-		key[0]++;
-	else
-		key[0] = 0;
-
-	if (CheckHitKey(KEY_INPUT_2))
-		key[1]++;
-	else
-		key[1] = 0;
-
-	if (CheckHitKey(KEY_INPUT_3))
-		key[2]++;
-	else
-		key[2] = 0;
-
-	if (CheckHitKey(KEY_INPUT_4))
-		key[3]++;
-	else
-		key[3] = 0;
-
-	if (CheckHitKey(KEY_INPUT_5))
-		key[4]++;
-	else
-		key[4] = 0;
+	int k = KEY_INPUT_1;
+	for (int i = 0; i < 5; i++) {
+		if (CheckHitKey(k++))
+			key[i]++;
+		else
+			key[i] = 0;
+	}
 }
 
 //Žæ“¾
