d50441f260884d6c8dbd33ad0a245a2a3648d590
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-08-23 01:09:25 +0900

    曲選択画面、前回と前々回の点数を表示
    サーバからデータを取得するため、Login.cppを作成（未実装）
    前回と前々回のデータを保存し、曲選択画面に表示

--- Mai-Kagami/Mai-Kagami/SongSelectCommon.h
+++ Mai-Kagami/Mai-Kagami/SongSelectCommon.h
@@ -8,7 +8,9 @@
 #include "SongSelectCover.h"
 #include "Songs.h"
 #include "SongSelectDefine.h"
+#include "Login.h"
 #include "Scene.h"
+#include "User.h"
 
 //曲選択画面タイトル
 class SongSelectTitle : public SubScene {
@@ -25,7 +27,7 @@ private:
 //曲選択画面カバー関係
 class SongInformation : public SubScene {
 public:
-	SongInformation(Font *font, Songs *songs, Touch *touch); //初期化
+	SongInformation(Font *font, Songs *songs, Touch *touch, User *user); //初期化
 	void Load();
 	void ContentView(); //表示
 	void ContentUpdate();
@@ -33,9 +35,11 @@ public:
 	~SongInformation();
 private:
 	int n, now;
+	User *user;
 	Touch *touch;
 	SongSelectCover *songCover[256];
 	Songs *songs;
+	Login *login;
 	SongSelectCover *nowSong;
 	MyDrawGraph *grad[2]; //カバー画像
 	MyDrawGraph *box; //カバー画像
