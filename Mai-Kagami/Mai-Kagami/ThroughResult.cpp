042f3d03b08d84e42f98f84f7c137c6b625ac0db
kousuke nezu <kousuke.n2132@gmail.com>
2016-09-01 00:26:23 +0900

    コード整理

--- Mai-Kagami/Mai-Kagami/ThroughResult.cpp
+++ Mai-Kagami/Mai-Kagami/ThroughResult.cpp
@@ -2,16 +2,16 @@
 
 ThroughResult::ThroughResult(Font *font, Songs *songs, Touch *touch) {
 	this->songs = songs;
-	title = new MyDrawTextLine(font, "採点結果", WIDTH * 0.5, HEIGHT * 0.15, 1, 60, WIDTH * 0.5, 4); //採点結果
-	circle = new MyDrawCircle(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 10, "WHITE"); //縁が白色の円
-	pointCircle = new MyDrawCircleGauge(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 78, 6); //青色の弧
+	title        = new MyDrawTextLine(font, "採点結果", WIDTH * 0.5, HEIGHT * 0.15, 1, 60, WIDTH * 0.5, 4); //採点結果
+	circle       = new MyDrawCircle(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 10, "WHITE"); //縁が白色の円
+	pointCircle  = new MyDrawCircleGauge(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 78, 6); //青色の弧
 	pointCircle2 = new MyDrawCircle(pointCircle->GetEndX() * SIZE_RATE, pointCircle->GetEndY() * SIZE_RATE, 16); //弧の先の円
-	button = new CircleButton2(font, touch, "次へ", 4); //次へボタン
-	text = new MyDrawText(font, "総合得点", WIDTH * 0.5, HEIGHT * 0.42, 1, 46, "White"); //「総合得点」
-	point1 = new MyDrawGraph(WIDTH * 0.42, HEIGHT * 0.51, "img/5.png", 0.23);
-	point2 = new MyDrawGraph(WIDTH * 0.51, HEIGHT * 0.51, "img/8.png", 0.23);
-	unit = new MyDrawText(font, "点", WIDTH * 0.58, HEIGHT * 0.54, 0, 46, "White"); //「点」
-	last = new MyDrawText(font, "前回 --点", WIDTH * 0.5, HEIGHT * 0.58, 1, 36, "White"); //前回の点数
+	button       = new CircleButton2(font, touch, "次へ", 4); //次へボタン
+	text         = new MyDrawText(font, "総合得点", WIDTH * 0.5, HEIGHT * 0.42, 1, 46, "White"); //「総合得点」
+	point1       = new MyDrawGraph(WIDTH * 0.42, HEIGHT * 0.51, "img/5.png", 0.23);
+	point2       = new MyDrawGraph(WIDTH * 0.51, HEIGHT * 0.51, "img/8.png", 0.23);
+	unit         = new MyDrawText(font, "点", WIDTH * 0.58, HEIGHT * 0.54, 0, 46, "White"); //「点」
+	last         = new MyDrawText(font, "前回 --点", WIDTH * 0.5, HEIGHT * 0.58, 1, 36, "White"); //前回の点数
 }
 
 void ThroughResult::Load() {
@@ -37,27 +37,30 @@ void ThroughResult::ContentUpdate() {
 }
 
 void ThroughResult::ContentView() {
-	title->View(); //タイトル表示
-	song->coverGraph->View(); //曲カバー画像表示
-	song->drawSongTitle->View(); //曲タイトル表示
-	circle->View(); //白色の円表示
-	pointCircle->View(); //青色の弧を表示
-	pointCircle2->View(); //弧の先の円を表示
-	text->View(); //「総合得点」表示
-	point1->View();
-	point2->View();
-	unit->View(); //「点」を表示
-	last->View(); //前回の得点を表示
-	button->View(); //ボタンを表示
+	title->View();					//タイトル表示
+	song->coverGraph->View();		//曲カバー画像表示
+	song->drawSongTitle->View();	//曲タイトル表示
+	circle->View();					//白色の円表示
+	pointCircle->View();			//青色の弧を表示
+	pointCircle2->View();			//弧の先の円を表示
+	text->View();					//「総合得点」表示
+	point1->View();					//得点(10の位)表示
+	point2->View();					//得点(1の位)表示
+	unit->View();					//「点」を表示
+	last->View();					//前回の得点を表示
+	button->View();					//ボタンを表示
 }
 
 ThroughResult::~ThroughResult() {
-	delete title; //タイトル削除
-	delete circle; //曲カバー画像削除
-	delete pointCircle; //青色の弧削除
-	delete pointCircle2; //弧の先の円削除
-	delete button; //ボタンを削除
-	delete text; //「総合得点」削除
-	delete unit; //「点」を削除
-	delete last; //前回の得点削除
+	delete title;					//タイトル削除
+	delete circle;					//曲カバー画像削除
+	delete pointCircle;				//青色の弧削除
+	delete pointCircle2;			//弧の先の円削除
+	delete button;					//ボタンを削除
+	delete text;					//「総合得点」削除
+	delete point1;					//得点(10の位)削除
+	delete point2;					//得点(1の位)削除
+	delete unit;					//「点」を削除
+	delete last;					//前回の得点削除
+	delete button;					//ボタン削除
 }
