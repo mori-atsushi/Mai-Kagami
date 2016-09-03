7874939a5c3cff1db5564fa109791d5d0e42d383
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-09-03 16:36:26 +0900

    PartOption.cpp周辺を修正
    部分練習ポーズ画面で設定の変更がしやすいように

--- Mai-Kagami/Mai-Kagami/PartOption.cpp
+++ Mai-Kagami/Mai-Kagami/PartOption.cpp
@@ -1,179 +1,29 @@
 #include "PartOption.h"
 
-PartOptionSpeedPop::PartOptionSpeedPop(Font *font, Songs *songs, Touch *touch)
-	: SpeedPop(font, songs, touch) {}
-
-int PartOptionSpeedPop::Switch(const int scene) {
-	if (button->GetTouch() == 1) {
-		song->danceMovie->SetSpeed();
-		return OPTION2;
-	}
-	return scene;
+PartOptionPreview2::PartOptionPreview2(Font *font, Songs *songs, Touch *touch) 
+	: PartOptionPreview(font, songs, touch, OPTION2, OPTION2_PART, OPTION2_SPEED) {
+	button[0] = new CircleButton(font, touch, "スタート!", 3, WIDTH * 0.8);
+	button[1] = new CircleButton2(font, touch, "戻る", 4);
 }
 
-void PartOptionSpeedPop::ContentUpdate() {
-	static int lastScene = MAIN;
-	if (nowScene == OPTION2_SPEED) {
-		if(lastScene == nowScene)
-			SpeedPop::ContentUpdate();
-		viewFlag = TRUE;
-	}
-	else {
-		viewFlag = FALSE;
-	}
-	lastScene = nowScene;
-}
-
-PartOptionPartPop::PartOptionPartPop(Font *font, Songs *songs, Touch *touch)
-	: PartPop(font, songs, touch) {}
-
-int PartOptionPartPop::Switch(const int scene) {
-	if (button->GetTouch() == 1) {
-		song->danceMovie->SetSpeed();
-		return OPTION2;
-	}
-	return scene;
-}
-
-void PartOptionPartPop::ContentUpdate() {
-	static int lastScene = MAIN;
-	if (nowScene == OPTION2_PART) {
-		if (lastScene == OPTION2)
-			PartPop::Init();
-		else if (lastScene == nowScene)
-			PartPop::ContentUpdate();
-		viewFlag = TRUE;
-	}
-	else {
-		viewFlag = FALSE;
-	}
-	lastScene = nowScene;
-}
-
-PartOptionPreview::PartOptionPreview(Font *font, Songs *songs, Touch *touch) {
-	this->songs = songs;
-	button[0] = new CircleButton(font, touch, "区間", 0, WIDTH * 0.8);
-	button[1] = new CircleButton(font, touch, "", 2, WIDTH * 0.8);
-	button[2] = new CircleButton(font, touch, "スタート!", 3, WIDTH * 0.8);
-	button[3] = new CircleButton2(font, touch, "戻る", 4);
-	message = new MyDrawText(font, "変更したいものを選んでください", WIDTH * 0.75, HEIGHT * 0.45, 1, 30);
-	caption[0] = new MyDrawText(font, "開始：", WIDTH * 0.8, HEIGHT * 0.53, 2, 30);
-	caption[1] = new MyDrawText(font, "終了：", WIDTH * 0.8, HEIGHT * 0.555, 2, 30);
-	caption[2] = new MyDrawText(font, "速度：", WIDTH * 0.8, HEIGHT * 0.6, 2, 30);
-	para[0] = new MyDrawText(font, "", WIDTH * 0.81, HEIGHT * 0.53, 0, 30, "Yellow");
-	para[1] = new MyDrawText(font, "", WIDTH * 0.81, HEIGHT * 0.555, 0, 30, "Yellow");
-	para[2] = new MyDrawText(font, "", WIDTH * 0.81, HEIGHT * 0.6, 0, 30, "Yellow");
-}
-
-int PartOptionPreview::Switch(const int scene) {
+int PartOptionPreview2::Switch(const int scene) {
 	if (button[0]->GetTouch() == 1)
-		return OPTION2_PART;
-	if (button[1]->GetTouch() == 1)
-		return OPTION2_SPEED;
-	if (button[2]->GetTouch() == 1)
 		return NEXT2;
-	if (button[3]->GetTouch() == 1)
+	if (button[1]->GetTouch() == 1)
 		return MODE;
-	return scene;
-}
-void PartOptionPreview::ContentUpdate() {
-	Song *song = songs->GetSong(songs->GetNowSong());
-	switch (nowScene)
-	{
-	case OPTION2:
-		char str[256];
-		sprintf_s(str, sizeof(str), "×%1.1lf", song->danceMovie->GetSpeed());
-		para[0]->ChangeText(song->GetPart(song->StartPart())->GetName());
-		para[1]->ChangeText(song->GetPart(song->EndPart())->GetName());
-		para[2]->ChangeText(str);
-		viewFlag = TRUE;
-		break;
-	default:
-		viewFlag = FALSE;
-		break;
-	}
+	return PartOptionPreview::Switch(scene);
 }
 
-void PartOptionPreview::ContentView() {
-	for (int i = 0; i < 4; i++)
+void PartOptionPreview2::ContentView() {
+	for (int i = 0; i < 2; i++)
 		button[i]->View();
-	message->View();
-	for (int i = 0; i < 3; i++) {
-		caption[i]->View();
-		para[i]->View();
-	}
+	PartOptionPreview::ContentView();
 }
 
-PartOptionPreview::~PartOptionPreview() {
-	for (int i = 0; i < 4; i++)
+PartOptionPreview2::~PartOptionPreview2() {
+	for (int i = 0; i < 2; i++)
 		delete button[i];
-	delete message;
-	for (int i = 0; i < 3; i++) {
-		delete caption[i];
-		delete para[i];
-	}
 }
 
-PartOptionButton::PartOptionButton(Font *font, Songs *songs, Touch *touch) {
-	this->songs = songs;
-	speedPop = new PartOptionSpeedPop(font, songs, touch);
-	partPop = new PartOptionPartPop(font, songs, touch);
-	partOptionPreview = new PartOptionPreview(font, songs, touch);
-}
-
-int PartOptionButton::Switch(const int scene) { 
-	switch (scene)
-	{
-	case OPTION2:
-		return partOptionPreview->Switch(scene);
-	case OPTION2_PART:
-		return partPop->Switch(scene);
-	case OPTION2_SPEED:
-		return speedPop->Switch(scene);
-	}
-	return scene;
-}
-
-void PartOptionButton::Load() {
-	speedPop->Load();
-	partPop->Load();
-	partOptionPreview->Load();
-}
-
-void PartOptionButton::Delete() {
-	speedPop->Delete();
-	partPop->Delete();
-	partOptionPreview->Delete();
-}
-
-void PartOptionButton::ContentUpdate() {
-	speedPop->Update(nowScene);
-	partPop->Update(nowScene);
-	partOptionPreview->Update(nowScene);
-	Song *song = songs->GetSong(songs->GetNowSong());
-	switch (nowScene)
-	{
-	case OPTION2:
-		song->ChangeStart(0);
-		song->ChangeEnd(0);
-	case OPTION2_PART:
-	case OPTION2_SPEED:
-		viewFlag = TRUE;
-		break;
-	default:
-		viewFlag = FALSE;
-		break;
-	}
-}
-
-void PartOptionButton::ContentView() {
-	speedPop->View();
-	partPop->View();
-	partOptionPreview->View();
-}
-
-PartOptionButton::~PartOptionButton() {
-	delete speedPop;
-	delete partPop;
-	delete partOptionPreview;
-}
+PartOptionButton::PartOptionButton(Font *font, Songs *songs, Touch *touch) 
+	: PartOptionPop(font, songs, touch, OPTION2, OPTION2_PART, OPTION2_SPEED, new PartOptionPreview2(font, songs, touch)) {}
--- Mai-Kagami/Mai-Kagami/PartOption.h
+++ Mai-Kagami/Mai-Kagami/PartOption.h
@@ -2,61 +2,22 @@
 #define __PARTOPTION_H_INCLUDED__
 
 #include "DxLib.h"
-#include "Scene.h"
-#include "Font.h"
-#include "Songs.h"
-#include "Song.h"
-#include "Touch.h"
+#include "PartOptionPop.h"
 #include "SongSelectDefine.h"
-#include "SeetingPop.h"
-#include "Button.h"
 
-//区間変更用ポップアップ
-class PartOptionSpeedPop : public SpeedPop {
+class PartOptionPreview2 : public PartOptionPreview {
 public:
-	PartOptionSpeedPop(Font *font, Songs *songs, Touch *touch);
+	PartOptionPreview2(Font *font, Songs *songs, Touch *touch);
 	int Switch(const int scene);
+	~PartOptionPreview2();
 private:
-	void ContentUpdate();
-};
-
-//スピード変更用ポップアップ
-class PartOptionPartPop : public PartPop {
-public:
-	PartOptionPartPop(Font *font, Songs *songs, Touch *touch);
-	int Switch(const int scene);
-private:
-	void ContentUpdate();
-};
-
-//オプション画面の動画とボタン
-class PartOptionPreview : public SubScene {
-public:
-	PartOptionPreview(Font *font, Songs *songs, Touch *touch);
-	int Switch(const int scene);
-	~PartOptionPreview();
-private:
-	Songs *songs;
-	void ContentUpdate();
 	void ContentView();
-	Button *button[4];
-	MyDrawText *message, *caption[3], *para[3];
+	Button *button[2];
 };
 
-class PartOptionButton : public SubScene {
+class PartOptionButton : public PartOptionPop {
 public:
 	PartOptionButton(Font *font, Songs *songs, Touch *touch);
-	int Switch(const int scene);
-	void Load();
-	void Delete();
-	~PartOptionButton();
-private:
-	Songs *songs;
-	void ContentUpdate();
-	void ContentView();
-	PartOptionSpeedPop *speedPop;
-	PartOptionPartPop *partPop;
-	PartOptionPreview *partOptionPreview;
 };
 
 
