e8128b0181634bcbdc5cb4ce06cdbdfd8bf066e4
kousuke nezu <kousuke.n2132@gmail.com>
2016-09-13 23:32:54 +0900

    ブランチ切ったあとのコミット

---
+++ Mai-Uploader/Mai_Uploader.java
@@ -0,0 +1,48 @@
+
+import java.io.File;
+import java.util.Calendar;
+
+/*
+ * 参考ページ
+ *  https://developers.google.com/youtube/v3/code_samples/java?hl=ja#upload_a_video 
+ */
+
+/*
+ * ファイル監視~youtubeアップロードまでのプロセスの説明をします
+ * 1.tempフォルダを空フォルダにする
+ * 2.videoフォルダを監視(dirMonitor.start())。ファイルが追加されたらtempに移動
+ * 3.mainクラスでtempフォルダを監視。
+ * 4.tempが空フォルダじゃなくなったらそのファイルのアップロードにうつる
+ * 5．アップロードが終わったらそのファイルを消し1に戻る(2については常に動いている)
+ */
+
+public class Mai_Uploader {
+	public static void main(String[] args) {
+		
+		//フォルダ監視用インスタンス作成(videoフォルダを監視)
+		DirMonitor dirMonitor = new DirMonitor("./video");
+		//監視開始
+		dirMonitor.start();
+		//tempフォルダが空フォルダじゃなくなるまでループ
+		File checkedFile = new File("\temp");
+		while(!checkedFile.exists()){	}
+		
+		//tempに追加されたファイルの取得
+		String[] fileList = checkedFile.list();
+		
+		MyYouTube myYoutube;
+		try {
+			myYoutube = new MyYouTube();
+			//アップロードするファイルを追加				
+			myYoutube.setVideo(new File(fileList[0]));
+				
+			//ファイルに情報を追加する
+			myYoutube.setMetadata(
+					"Mai_Uploader test upload on "  + Calendar.getInstance().getTime(),
+					"舞鏡の動画のテストアップロード " + "on " + Calendar.getInstance().getTime()
+					);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+ 	}
+}
