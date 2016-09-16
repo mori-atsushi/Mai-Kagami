59b48f4410d8fdbd9a45c03b108c7dd16c90ad81
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-09-16 11:05:46 +0900

    録画用プロジェクトファイル作成

---
+++ Record/Record/Main.cpp
@@ -0,0 +1,16 @@
+#include "Main.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	SetGraphMode(WIDTH, HEIGHT, 32); //ウィンドウサイズ設定
+	SetBackgroundColor(0, 0, 0); //背景色設定
+	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
+	SetAlwaysRunFlag(TRUE); //バックグラウンドでも処理を実行
+
+	// while(裏画面を表画面に反映, メッセージ処理, 画面クリア)
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+	}
+
+	DxLib_End(); // DXライブラリ終了処理
+
+	return 0;
+}
---
+++ Record/Record/Main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H_INCLUDED__
+#define __MAIN_H_INCLUDED__
+
+#include <Kinect.h>
+#include "DxLib.h"
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+#endif
