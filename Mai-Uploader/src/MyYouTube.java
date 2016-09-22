e678486fba577bc328fb4477614b7261b3a22d42
kousuke nezu <kousuke.n2132@gmail.com>
2016-09-22 15:09:46 +0900

    try-catch文をスローに書き換え

--- Mai-Uploader/src/MyYouTube.java
+++ Mai-Uploader/src/MyYouTube.java
@@ -33,160 +33,145 @@ public class MyYouTube {
 	private int fileSize;
 	private int range=0;
 	
-	public void setMetadata(File video, String title, String description){
+	public void setMetadata(File video, String title, String description) throws IOException{
 		//渡されたファイルがavi形式でなければ何もせずreturn
 		if(!video.getName().endsWith(".avi")) return;
 		
 		//アクセストークンの取得
-		try{
-			URL access_url = new URL(TOKEN_URL);
-            URLConnection access_uc = access_url.openConnection();
-            access_uc.setDoOutput(true);
-            String postStr = "grant_type=refresh_token&refresh_token="+ REFRESH_TOKEN + "&client_id=" + CONSUMER_KEY + "&client_secret=" + CONSUMER_SECRET;
-            OutputStream access_os = access_uc.getOutputStream();
-            PrintStream access_ps = new PrintStream(access_os);
-            access_ps.print(postStr);
-            access_ps.close();
+		URL access_url = new URL(TOKEN_URL);
+		URLConnection access_uc = access_url.openConnection();
+		access_uc.setDoOutput(true);
+		String postStr = "grant_type=refresh_token&refresh_token="+ REFRESH_TOKEN + "&client_id=" + CONSUMER_KEY + "&client_secret=" + CONSUMER_SECRET;
+		OutputStream access_os = access_uc.getOutputStream();
+		PrintStream access_ps = new PrintStream(access_os);
+		access_ps.print(postStr);
+		access_ps.close();
 
-            //レスポンスの取得
-            InputStream access_is = access_uc.getInputStream();
-            BufferedReader access_reader = new BufferedReader(new InputStreamReader(access_is));
-            String json = null;
-            String s = null;
-            while ((s = access_reader.readLine()) != null) {
-            	json += s;
-            }
-            access_reader.close();
-            //jsonをjavaに変換
-            Code code = JSON.decode(json.substring(json.indexOf("{")), Code.class);
-            access_token = code.getAccess_token();
-		} catch (MalformedURLException e) {
-			System.out.println(e);
-            return;
-        } catch (IOException e) {
-        	System.out.println(e);
-            return;
-        }
+		//レスポンスの取得
+		InputStream access_is = access_uc.getInputStream();
+		BufferedReader access_reader = new BufferedReader(new InputStreamReader(access_is));
+		String json = null;
+		String s = null;
+		while ((s = access_reader.readLine()) != null) {
+			json += s;
+		}
+		access_reader.close();
+		//jsonをjavaに変換
+		Code code = JSON.decode(json.substring(json.indexOf("{")), Code.class);
+		access_token = code.getAccess_token();
 		
-		try {
-			//ファイルをバイナリに変換する
-			byte[] b = new byte[1];
-		    FileInputStream fis = new FileInputStream(video);
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		    while (fis.read(b) > 0) { baos.write(b); }
-		    baos.close();
-		    fis.close();
-		    b = baos.toByteArray();
-		    int[] fileByte = new int[b.length];
-		    //byteは負の数を含んでしまうのですべて正の数になるよう変換
-		    for(int i = 0; i < b.length; i++){
-		    	fileByte[i] = Byte.toUnsignedInt(b[i]);
-		    }
-		    this.fileSize = fileByte.length;
-		    //再開可能なアップロードのセッションを開始するためのurl
-			URL resume_url = new URL("https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable&part=snippet,status,contentDetails&key=AIzaSyDEgkuehdEK_kh0gEw6-3c-8WwcIS2i0nI");
-			//タイトル、説明などの情報を持つjson
-			String sentJson = "{\"snippet\": {\"title\": \"" + title + "\",\"description\": \"" + description + "\"},\"status\": {\"privacyStatus\": \"unlisted\",\"embeddable\": \"True\",\"license\": \"youtube\"}}";
-			HttpURLConnection resume_uc = (HttpURLConnection) resume_url.openConnection();
-			resume_uc.setUseCaches(false);			// キャッシュを使用しない
-			resume_uc.setDoOutput(true);			// アウトプットできるようにする
-			resume_uc.setRequestMethod("POST");		// ポスト通信である
-     
-			//ヘッダの設定
-			resume_uc.setRequestProperty("HOST", "www.googleapis.com");
-			resume_uc.setRequestProperty("Authorization", "Bearer " + access_token);
-			resume_uc.setRequestProperty("Content-Length", String.valueOf(sentJson.length()));
-			resume_uc.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			resume_uc.setRequestProperty("X-Upload-Content-Length", String.valueOf(fileSize));
-			resume_uc.setRequestProperty("X-upload-content-type", "video/avi");
+		//ファイルをバイナリに変換する
+		byte[] b = new byte[1];
+		FileInputStream fis = new FileInputStream(video);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		while (fis.read(b) > 0) { baos.write(b); }
+		baos.close();
+		fis.close();
+		b = baos.toByteArray();
+		int[] fileByte = new int[b.length];
+		//byteは負の数を含んでしまうのですべて正の数になるよう変換
+		for(int i = 0, n = b.length; i < n; i++){
+			fileByte[i] = Byte.toUnsignedInt(b[i]);
+		}
+		this.fileSize = fileByte.length;
+		//再開可能なアップロードのセッションを開始するためのurl
+		URL resume_url = new URL("https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable&part=snippet,status,contentDetails&key=AIzaSyDEgkuehdEK_kh0gEw6-3c-8WwcIS2i0nI");
+		//タイトル、説明などの情報を持つjson
+		String sentJson = "{\"snippet\": {\"title\": \"" + title + "\",\"description\": \"" + description + "\"},\"status\": {\"privacyStatus\": \"unlisted\",\"embeddable\": \"True\",\"license\": \"youtube\"}}";
+		HttpURLConnection resume_uc = (HttpURLConnection) resume_url.openConnection();
+		resume_uc.setUseCaches(false);			// キャッシュを使用しない
+		resume_uc.setDoOutput(true);			// アウトプットできるようにする
+		resume_uc.setRequestMethod("POST");		// ポスト通信である
 			
-			OutputStream resume_os = resume_uc.getOutputStream();	
-			PrintStream resume_ps = new PrintStream(resume_os);
-			resume_ps.print(sentJson);
-			resume_ps.close();
-			resume_os.close();
-			//実際に動画のアップロード
-			URL upload_url = new URL(resume_uc.getHeaderField("location"));
-			HttpURLConnection upload_uc = (HttpURLConnection)upload_url.openConnection();
-			upload_uc.setUseCaches(false);
-			upload_uc.setDoOutput(true);
-			upload_uc.setRequestMethod("POST");
-			
-			//ヘッダの設定
-			upload_uc.setRequestProperty("Authorization", "Bearer " + access_token);
-			upload_uc.setRequestProperty("Content-Length", String.valueOf(fileSize));
-			upload_uc.setRequestProperty("Content-Type", "video/avi");
-			OutputStream upload_os = upload_uc.getOutputStream();	
-			PrintStream upload_ps = new PrintStream(upload_os);
-			//送信
-			for(int i :fileByte)upload_ps.write(i);
-			upload_ps.close();	upload_os.close();
-            //レスポンスの取得
-            InputStream is = upload_uc.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String s;
-            String upload_json = null;
-            while((s = br.readLine()) != null){
-            	upload_json += s;
-            	System.out.println(s);
-            }
-            is.close(); br.close();
-            if(upload_uc.getHeaderField("Range") != null){
-            	this.range = Integer.parseInt(upload_uc.getHeaderField("Range").substring(upload_uc.getHeaderField("Range").indexOf("-") + 1));
-            }else{
-            	this.range = 0;
-            }
+		//ヘッダの設定
+		resume_uc.setRequestProperty("HOST", "www.googleapis.com");
+		resume_uc.setRequestProperty("Authorization", "Bearer " + access_token);
+		resume_uc.setRequestProperty("Content-Length", String.valueOf(sentJson.length()));
+		resume_uc.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		resume_uc.setRequestProperty("X-Upload-Content-Length", String.valueOf(fileSize));
+		resume_uc.setRequestProperty("X-upload-content-type", "video/avi");
+		
+		OutputStream resume_os = resume_uc.getOutputStream();	
+		PrintStream resume_ps = new PrintStream(resume_os);
+		resume_ps.print(sentJson);
+		resume_ps.close();
+		resume_os.close();
+		//実際に動画のアップロード
+		URL upload_url = new URL(resume_uc.getHeaderField("location"));
+		HttpURLConnection upload_uc = (HttpURLConnection)upload_url.openConnection();
+		upload_uc.setUseCaches(false);
+		upload_uc.setDoOutput(true);
+		upload_uc.setRequestMethod("POST");
+		
+		//ヘッダの設定
+		upload_uc.setRequestProperty("Authorization", "Bearer " + access_token);
+		upload_uc.setRequestProperty("Content-Length", String.valueOf(fileSize));
+		upload_uc.setRequestProperty("Content-Type", "video/avi");
+		OutputStream upload_os = upload_uc.getOutputStream();	
+		PrintStream upload_ps = new PrintStream(upload_os);
+		//送信
+		for(int i :fileByte)upload_ps.write(i);
+		upload_ps.close();	upload_os.close();
+		//レスポンスの取得
+		InputStream is = upload_uc.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String upload_json = null;
+		while((s = br.readLine()) != null){
+			upload_json += s;
+			System.out.println(s);
+		}
+		is.close(); br.close();
+		if(upload_uc.getHeaderField("Range") != null){
+			this.range = Integer.parseInt(upload_uc.getHeaderField("Range").substring(upload_uc.getHeaderField("Range").indexOf("-") + 1));
+		}else{
+			this.range = 0;
+        }
             
-            while(true){
-            	//レスポンスコードの取得
-            	int rescode = upload_uc.getResponseCode();
-            	//Range(どこまでアップロードできたかの情報)を取得
-            	//再開可能なレスポンスコードでRangeがナルでないとき
-            	if((rescode == 308 || rescode == 503) && this.range != 0){
-            		System.out.println(getHeader(
+		while(true){
+			//レスポンスコードの取得
+			int rescode = upload_uc.getResponseCode();
+			//Range(どこまでアップロードできたかの情報)を取得
+			//再開可能なレスポンスコードでRangeがナルでないとき
+			if((rescode == 308 || rescode == 503) && this.range != 0){
+				System.out.println(getHeader(
             				resumeReqest(
             						upload_url, 
             						Arrays.copyOfRange(fileByte, range + 1, 3971110),
             						(int)fileSize, 
             						range)));
-            	}else if(rescode == 200){
-            		Code info_code = JSON.decode(upload_json.substring(upload_json.indexOf("{")), Code.class);
-            		String infostr =  video.getName().substring(0,video.getName().indexOf("."));
-            		String[] infolist = infostr.split("-", 0);
-            		String params = null;
-            		if(infolist.length > 2){
-            			params = "user="+infolist[0]+"&song="+infolist[1]+"&date="+infolist[2]+"&movie="+info_code.getId();
-            		}
-            		URL info_url = new URL("http://localhost:8888/test/Move.php?"+params);
-            		HttpURLConnection info_uc = (HttpURLConnection) info_url.openConnection();
-            		info_uc.setUseCaches(false);			// キャッシュを使用しない
-        			info_uc.setDoOutput(true);			// アウトプットできるようにする
-        			info_uc.setRequestMethod("POST");		// ポスト通信である
-            		OutputStream info_os = info_uc.getOutputStream();
-            		PrintStream info_ps = new PrintStream(info_os);
-            		info_ps.close();	info_os.close();
-            		
-            		InputStream info_is = info_uc.getInputStream();
-            		BufferedReader info_br = new BufferedReader(new InputStreamReader(info_is));
-            		String l = null;
-            		while((l = info_br.readLine()) != null){
-            			System.out.println(l);
-            		}
-            		info_br.close();	info_is.close();
-            		return;
-            	}else{
-            		resume_ps.close();
-            		br.close();
-            		baos.close();
-            		upload_ps.close();
-            		return;
+			}else if(rescode == 200){
+				Code info_code = JSON.decode(upload_json.substring(upload_json.indexOf("{")), Code.class);
+				String infostr =  video.getName().substring(0,video.getName().indexOf("."));
+				String[] infolist = infostr.split("-", 0);
+				String params = null;
+				if(infolist.length > 2){
+					params = "user="+infolist[0]+"&song="+infolist[1]+"&date="+infolist[2]+"&movie="+info_code.getId();
             	}
+				URL info_url = new URL("http://localhost:8888/test/Move.php?"+params);
+				HttpURLConnection info_uc = (HttpURLConnection) info_url.openConnection();
+				info_uc.setUseCaches(false);			// キャッシュを使用しない
+				info_uc.setDoOutput(true);			// アウトプットできるようにする
+				info_uc.setRequestMethod("POST");		// ポスト通信である
+				OutputStream info_os = info_uc.getOutputStream();
+				PrintStream info_ps = new PrintStream(info_os);
+				info_ps.close();	info_os.close();
+				
+				InputStream info_is = info_uc.getInputStream();
+				BufferedReader info_br = new BufferedReader(new InputStreamReader(info_is));
+				String l = null;
+				while((l = info_br.readLine()) != null){
+            			System.out.println(l);
+				}
+				info_br.close();	info_is.close();
+				return;
+            }else{
+            	resume_ps.close();
+            	br.close();
+            	baos.close();
+            	upload_ps.close();
+            	return;
             }
-		}catch(MalformedURLException e){
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        }
 	}
 
 	/**
@@ -195,46 +180,41 @@ public class MyYouTube {
 	 * @param file		アップロードするファイル
 	 * @param fileSize	アップロードするファイルのサイズ
 	 * @param range		これまでアップロードできた範囲
+	 * @throws IOException 
 	 */
-	private HttpURLConnection resumeReqest(URL url, final int[] file, int fileSize, int range){
+	private HttpURLConnection resumeReqest(URL url, final int[] file, int fileSize, int range) throws IOException{
 		int firstRange = range + 1;
 		int lastRange = fileSize - 1;
 		HttpURLConnection uc = null;
-		try {
-			uc = (HttpURLConnection)url.openConnection();
-			uc.setUseCaches(false);
-			uc.setDoOutput(true);
-			uc.setRequestMethod("POST");
-			
-			uc.setRequestProperty("Authorization", "Bearer " + access_token);
-			uc.setRequestProperty("Content-Length", String.valueOf(fileSize - firstRange));
-			System.out.println(fileSize-firstRange);
-			uc.setRequestProperty("Content-Range", "bytes " + String.valueOf(firstRange) + "-" + String.valueOf(lastRange) + "/" + String.valueOf(fileSize));
-			OutputStream os = uc.getOutputStream();
-			PrintStream ps = new PrintStream(os);
-			for(int i :file){
-				ps.write(i);
-			}
-            ps.close();	os.close();
-            if(uc.getHeaderField("Range") != null){
-            	this.range = Integer.parseInt(uc.getHeaderField("Range").substring(uc.getHeaderField("Range").indexOf("-") + 1));
-            }
-            InputStream is = uc.getErrorStream();
-            if(is != null){
-            	BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            	String s = null;
-            	while((s = br.readLine()) != null){
-            	System.out.println(s);
-            	br.close();
-            	}
-            }
-            is.close();
-            return uc;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return uc;
-		}
+		uc = (HttpURLConnection)url.openConnection();
+		uc.setUseCaches(false);
+		uc.setDoOutput(true);
+		uc.setRequestMethod("POST");
 		
+		uc.setRequestProperty("Authorization", "Bearer " + access_token);
+		uc.setRequestProperty("Content-Length", String.valueOf(fileSize - firstRange));
+		System.out.println(fileSize-firstRange);
+		uc.setRequestProperty("Content-Range", "bytes " + String.valueOf(firstRange) + "-" + String.valueOf(lastRange) + "/" + String.valueOf(fileSize));
+		OutputStream os = uc.getOutputStream();
+		PrintStream ps = new PrintStream(os);
+		for(int i :file){
+			ps.write(i);
+		}
+		ps.close();	os.close();
+		if(uc.getHeaderField("Range") != null){
+			this.range = Integer.parseInt(uc.getHeaderField("Range").substring(uc.getHeaderField("Range").indexOf("-") + 1));
+		}
+		InputStream is = uc.getErrorStream();
+		if(is != null){
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String s = null;
+			while((s = br.readLine()) != null){
+				System.out.println(s);
+            	br.close();
+            }
+		}
+		is.close();
+		return uc;
 	}
 	
 	//レスポンスヘッダの取得
