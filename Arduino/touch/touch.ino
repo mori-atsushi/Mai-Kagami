f4e11116fae0a09e60526a88732e4f69ebf1d135
Mori-Atsushi <e1237@s.akashi.ac.jp>
2016-10-02 15:48:28 +0900

    サイドタッチスイッチ実装完了

--- Arduino/touch/touch.ino
+++ Arduino/touch/touch.ino
@@ -1,40 +1,62 @@
-int f[5];
+#define NUM 50
+int f[NUM][5] ,n;
+int limit[6] = {5, 4, 5, 8, 3, 3}; //しきい値
 
 void setup(){
-  int i;
+  int i, j;
   //モニタリングのためシリアル通信開始
   Serial.begin(9600);
-  for(i = 0; i < 5; i++) {
+  for(i = 1; i <= 5; i++) {
     //ピンをデジタル出力
     pinMode(i * 2,OUTPUT);
     //ピンをデジタル入力
     pinMode(i * 2 + 1,INPUT);    
   }
+  for(i = 0; i < NUM; i++) {
+    for(j = 0; j < 5; j++)
+      f[i][j] = 0;
+  }
+  n = 0;
 }
 
 void loop(){
-  int limit = 5; //しきい値
  //静電容量変化量の変数を用意
-  int i, x;
-  long int num = 100000, digit = 10000;
+  int i, j, x;
+  long int num = 0, digit = 1;
+    char data; //送信データ
   
   //指が触れたとき9番ピンがHIGHになるまでをカウント
-  for(i = 0; i < 5; i++) {
+  for(i = 1; i <= 5; i++) {
     x = 0;
     //ピンをHIGHで出力
     digitalWrite(i * 2, HIGH);
     while (digitalRead(i * 2 + 1)!=HIGH){
       //カウントする
       x++;
-      if(x > 20)
+      if(x > limit[i])
+       break;
+    }
+    if(x > limit[i]) {
+      f[n][i] = 1;
+    } else {
+      f[n][i] = 0;
+    }
+    n++;
+    n%=NUM;
+    
+    for(j = 0; j < NUM; j++) {
+      if(f[j][i] == 1)
         break;
     }
-    f[i]+=(x-f[i])/2;
-    if(f[i] > limit)
-      num += digit;
-    digit *= 10;
+    if(j != NUM)
+      num += digit; 
+    digit *= 2;
     //ピンをLOWにする
     digitalWrite(i * 2, LOW);
   }
-  Serial.println(num);
+  if(num < 10)
+    data = '0' + num;
+  else
+    data = 'A' + num - 10;
+  Serial.println(data);
 }
